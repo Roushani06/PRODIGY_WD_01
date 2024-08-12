f52634aa440b654253dc033d5104cbfe7793847f
Roushani06 <roushani77100@gmail.com>
2024-08-12 12:39:21 +0530

    SCRIPT.JS

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let lines = document.querySelector(".lines");
+let berger = document.querySelector(".lines");
 let menu = document.querySelector(".nav-menu");
 let navbar = document.querySelector(".navbar");
 
-lines.addEventListener("click", () => {
+berger.addEventListener("click", () => {
   menu.classList.toggle("hide-res");
   navbar.classList.toggle("h-nav-resp");
 });
